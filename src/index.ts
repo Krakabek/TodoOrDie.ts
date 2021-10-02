ea4dba52a3d9bb54072315a1a7ca81e207fe395e
Danil Radkovskyi <v.danylo.radkovskyi@reddit.com>
2021-10-02 13:52:27 +0300

    Display error when TODO is overdue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export const TodoOrDie = (message: string) => {
-  console.error(message);
+export const TodoOrDie = (message: string, deadline: Date | string) => {
+  const currentDate = new Date();
+  const overdueDate = typeof deadline === "string" ? new Date(deadline) : deadline;
+  if (currentDate > overdueDate) {
+    console.error(`TODO: '${message}' is overdue. Do it!`);
+  }
 };
