414a11f4a924efb2e6f2c94addfa536cd0024ed5
Danil Radkovskyi <v.danylo.radkovskyi@reddit.com>
2021-10-02 13:56:09 +0300

    Use type overload for better typing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export const TodoOrDie = (message: string, deadline: Date | string) => {
+export function TodoOrDie(message: string, deadline: Date): void;
+export function TodoOrDie(message: string, deadline: string): void;
+export function TodoOrDie(message: string, deadline: Date | string): void {
   const currentDate = new Date();
   const overdueDate = typeof deadline === "string" ? new Date(deadline) : deadline;
   if (currentDate > overdueDate) {
     console.error(`TODO: '${message}' is overdue. Do it!`);
   }
-};
+}
